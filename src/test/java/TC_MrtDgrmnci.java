bd8bfd1586e80abbc18f372a8759eae6bcf445c8
Murat <degirmenci_murat@yahoo.com>
2020-11-01 15:33:45 -0500

    MrtDgrmnci

---
+++ src/test/java/TC_MrtDgrmnci.java
@@ -0,0 +1,17 @@
+
+
+public class TC_MrtDgrmnci {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Allrayyddiiiiiii :)))");
+
+        char[] mrt={'A','L','R','A','Y','D','I','I'};
+
+        System.out.println(mrt);
+
+
+    }
+
+}
